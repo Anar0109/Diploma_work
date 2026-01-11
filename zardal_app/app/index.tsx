234a71565313ab35306326e6def80d279e660cef
Anar <anar.tuvshinjargal@gmail.com>
2026-01-12 00:49:50 +0800

    day4

--- zardal_app/app/index.tsx
+++ zardal_app/app/index.tsx
@@ -1,11 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import {colors} from '@/constants/theme'
+import { Image, StyleSheet, Text, View } from "react-native"
+import {useEffect} from 'react';
+import React from 'react';
+import {colors} from '@/constants/theme';
+import {useRouter} from "expo-router";
 
 const index = () => {
+  const router = useRouter();
+  useEffect(()=>{
+    setTimeout(()=>{
+      router.push("/(auth)/welcome")
+    }, 2000);
+  },[])
   return (
     <View style={styles.container}>
-      <Text>index</Text>
+      <Image
+        style={styles.logo}
+        resizeMode="contain"
+        source={require('../assets/images/splashImage.png')}
+      />
     </View>
   )
 }
@@ -18,5 +30,11 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: colors.neutral900,
+    },
+
+    logo: {
+      height: "20%",
+      aspectRatio: 1,
+
     }
 })
